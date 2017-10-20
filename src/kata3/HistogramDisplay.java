1ceb09fa2560aa85e9a563828b70cd063881d555
usuario <usuario@10.22.148.62>
2017-10-20 10:35:34 +0100

    KATA3 VERSION 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -1,4 +1,3 @@
-
 package kata3;
 
 import java.awt.Dimension;
@@ -11,9 +10,11 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends  ApplicationFrame{
-
-    public HistogramDisplay() {
+    private final Histogram<String> histogram;
+    
+    public HistogramDisplay(Histogram<String> histogram) {
         super("HISTOGRAMA");
+        this.histogram = histogram;
         setContentPane(createPanel()); 
         pack();
     }
@@ -23,8 +24,7 @@ public class HistogramDisplay extends  ApplicationFrame{
     }
     
     private JPanel createPanel(){
-        ChartPanel chartPanel = new ChartPanel(
-                createChart(createDataset()));
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
         setPreferredSize(new Dimension(500,400));
         return chartPanel;
     }
@@ -43,11 +43,12 @@ public class HistogramDisplay extends  ApplicationFrame{
         return chart;
     }
     private DefaultCategoryDataset createDataset(){
-        DefaultCategoryDataset dataSet = 
-                new DefaultCategoryDataset();
-        dataSet.addValue(12, "", "gmail.com");
-        dataSet.addValue(64, "", "hotmail.com");
-        dataSet.addValue(567, "", "ulpgc.es");
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
+        
         return dataSet;
     }
         
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -2,8 +2,19 @@ package kata3;
 
 public class Kata3 {
 
-    public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay();
-        histo.execute();
+public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("google.es");
+        histogram.increment("gmail.es");
+        histogram.increment("google.com");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        
+        new HistogramDisplay(histogram).execute();
     }
 }
